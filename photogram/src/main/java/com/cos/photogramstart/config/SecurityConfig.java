3afbcdf7da4db1ae5b26523478da0a8f7bc579ff
이혜인 <leehi9817@gmail.com>
2024-03-06 13:06:47 +0900

    로그인 완료 및 View 연결

---
+++ photogram/src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.cos.photogramstart.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration // IoC
+public class SecurityConfig {
+	
+	@Bean
+	BCryptPasswordEncoder encode() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	SecurityFilterChain configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.authorizeRequests()
+			.antMatchers("/","/user/**","/image/**","/subscribe/**","/comment/**").authenticated()
+			.anyRequest().permitAll()
+			.and()
+			.formLogin()
+			.loginPage("/auth/signin") //GET
+			.loginProcessingUrl("/auth/signin") //POST -> 스프링 시큐리티가 로그인 프로세스 진행
+			.defaultSuccessUrl("/");
+		return http.build();
+	}
+	
+	/*@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// super 삭제 - 기존 시큐리티가 가지고 있는 기능이 다 비활성화됨.
+		http.csrf().diable();
+		http.authorizeRequests()
+		.antMatchers("/","/user/**","/image/**","/subscribe/**","/comment/**").authenticated()
+		.anyRequest().permitAll()
+		.and()
+		.formLogin()
+		.loginPage("/auth/signin")
+		.defaultSuccessUrl("/");*/
+}
---
+++ photogram/src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.cos.photogramstart.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cos.photogramstart.handler.ex.CustomValidationException;
+import com.cos.photogramstart.util.Script;
+
+@RestController
+@ControllerAdvice
+public class ControllerExceptionHandler {
+	@ExceptionHandler(CustomValidationException.class)
+	public String validationException(CustomValidationException e) {
+		// CMRespDto, Script 비교
+		// 1.클라이언트에게 응답할 때는 Script
+		// 2.Ajax 통신 - CMRespDto
+		// 3.Android 통신 - CMRespDto
+		return Script.back(e.getErrorMap().toString());
+	}
+}
