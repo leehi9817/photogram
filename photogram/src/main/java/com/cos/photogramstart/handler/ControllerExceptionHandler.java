a83e429cbcfac878daeb3740d0d89de5d9dd6ec1
이혜인 <leehi9817@gmail.com>
2024-03-08 18:27:32 +0900

    유효성 검사

--- photogram/src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ photogram/src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -1,15 +1,21 @@
 package com.cos.photogramstart.handler;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cos.photogramstart.handler.ex.CustomValidationApiException;
 import com.cos.photogramstart.handler.ex.CustomValidationException;
 import com.cos.photogramstart.util.Script;
+import com.cos.photogramstart.web.dto.CMRespDto;
 
 @RestController
 @ControllerAdvice
 public class ControllerExceptionHandler {
+	
+	// 자바 스크립트 리턴
 	@ExceptionHandler(CustomValidationException.class)
 	public String validationException(CustomValidationException e) {
 		// CMRespDto, Script 비교
@@ -17,5 +23,11 @@ public class ControllerExceptionHandler {
 		// 2.Ajax 통신 - CMRespDto
 		// 3.Android 통신 - CMRespDto
 		return Script.back(e.getErrorMap().toString());
+	}
+	
+	// 데이터 리턴(Ajax)
+	@ExceptionHandler(CustomValidationApiException.class)
+	public ResponseEntity<CMRespDto<?>> validationApiException(CustomValidationApiException e) {
+		return new ResponseEntity<>(new CMRespDto<>(-1,e.getMessage(),e.getErrorMap()),HttpStatus.BAD_REQUEST);
 	}
 }
--- photogram/src/main/resources/static/js/update.js
+++ photogram/src/main/resources/static/js/update.js
@@ -1,5 +1,6 @@
 // (1) 회원정보 수정
-function update(userId) {
+function update(userId,event) {
+	event.preventDefault(); // 폼태그 액션을 막기
 	
 	let data = $("#profileUpdate").serialize();
 	
@@ -11,10 +12,11 @@ function update(userId) {
 		data:data,
 		contentType:"application/x-www-form-urlencoded; charset=UTF-8",
 		dataType:"json"
-	}).done(res=>{
-		console.log("update 성공");
+	}).done(res=>{ // HttpStatus 상태코드 200번대
+		console.log("성공",res);
 		location.href=`/user/${userId}`;
-	}).fail(error=>{
-		console.log("update 실패")
+	}).fail(error=>{ // HttpStatus 상태코드 200번대가 아닐 때
+		alert(JSON.stringify(error.responseJSON.data));
+		console.log("실패",error.responseJSON.data);
 	});
 }
