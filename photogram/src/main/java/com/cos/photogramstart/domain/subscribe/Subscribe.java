e6b9ed2b0f92224acee6e7d22e6a6eb236b5b6c7
이혜인 <leehi9817@gmail.com>
2024-03-10 16:29:45 +0900

    구독 모델과 API 제작 및 예외처리

---
+++ photogram/src/main/java/com/cos/photogramstart/domain/subscribe/Subscribe.java
@@ -0,0 +1,55 @@
+package com.cos.photogramstart.domain.subscribe;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+//JPA - Java Persistence API (자바로 데이터를 영구적으로 저장(DB)할 수 있는 API를 제공)
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import com.cos.photogramstart.domain.user.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(
+		uniqueConstraints = {
+				@UniqueConstraint(
+						name="subscribe_uk",
+						columnNames = {"fromUserId","toUserId"} // 컬럼명 입력
+					)
+		}
+)
+public class Subscribe {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@JoinColumn(name = "fromUserId") // 컬럼명 지정
+	@ManyToOne
+	private User fromUser;
+	
+	@JoinColumn(name = "toUserId")
+	@ManyToOne
+	private User toUser;
+	
+	private LocalDateTime createDate;
+	
+	@PrePersist // DB에 INSERT 되기 직전에 실행
+	public void createDate() {
+		this.createDate = LocalDateTime.now();
+	}
+}
