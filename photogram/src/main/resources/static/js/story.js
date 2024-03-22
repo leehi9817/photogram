973822dc6bf3a8fa456a3c918e654ba3511c614e
이혜인 <leehi9817@gmail.com>
2024-03-22 20:13:49 +0900

    댓글쓰기 완성과 뷰 렌더링

--- photogram/src/main/resources/static/js/story.js
+++ photogram/src/main/resources/static/js/story.js
@@ -63,28 +63,33 @@ function getStoryItem(image) {
 			<p>${image.caption}</p>
 		</div>
 
-		<div id="storyCommentList-1">
+		<div id="storyCommentList-${image.id}">`;
 
-			<div class="sl__item__contents__comment" id="storyCommentItem-1"">
-				<p>
-					<b>Lovely :</b> 부럽습니다.
-				</p>
+		image.comments.forEach((comment)=>{
+			item += `<div class="sl__item__contents__comment" id="storyCommentItem-${comment.id}">
+			<p>
+				<b>${comment.user.username} :</b> ${comment.content}
+			</p>
 
-				<button>
-					<i class="fas fa-times"></i>
-				</button>
+			<button>
+				<i class="fas fa-times"></i>
+			</button>
 
-			</div>
+		</div>`;
+		
+		});
+		
+			
 
-		</div>
+		item += `</div>
 
 		<div class="sl__item__input">
-			<input type="text" placeholder="댓글 달기..." id="storyCommentInput-1" />
-			<button type="button" onClick="addComment()">게시</button>
+			<input type="text" placeholder="댓글 달기..." id="storyCommentInput-${image.id}" />
+			<button type="button" onClick="addComment(${image.id})">게시</button>
 		</div>
 
-	</div>
-</div>`;
+		</div>
+	</div>`;
 
 	return item;
 }
@@ -151,31 +156,48 @@ function toggleLike(imageId) {
 }
 
 // (4) 댓글쓰기
-function addComment() {
+function addComment(imageId) {
 
-	let commentInput = $("#storyCommentInput-1");
-	let commentList = $("#storyCommentList-1");
+	let commentInput = $(`#storyCommentInput-${imageId}`);
+	let commentList = $(`#storyCommentList-${imageId}`);
 
 	let data = {
+		imageId: imageId,
 		content: commentInput.val()
 	}
-
+	
 	if (data.content === "") {
 		alert("댓글을 작성해주세요!");
 		return;
 	}
 
-	let content = `
-			  <div class="sl__item__contents__comment" id="storyCommentItem-2""> 
-			    <p>
-			      <b>GilDong :</b>
-			      댓글 샘플입니다.
-			    </p>
-			    <button><i class="fas fa-times"></i></button>
-			  </div>
-	`;
-	commentList.prepend(content);
-	commentInput.val("");
+	$.ajax({
+		type:"post",
+		url:"/api/comment",
+		data:JSON.stringify(data),
+		contentType:"application/json; charset=utf-8",
+		dataType:"json"
+	}).done(res=>{
+		// console.log("성공", res);
+	
+		let comment = res.data;
+		
+		let content = `
+		  <div class="sl__item__contents__comment" id="storyCommentItem-${comment.id}"> 
+		    <p>
+		      <b>${comment.user.username} :</b>
+			     ${comment.content}
+		    </p>
+		    <button><i class="fas fa-times"></i></button>
+		  </div>
+		`;
+		
+		commentList.prepend(content);
+	}).fail(error=>{
+		console.log("오류", error);
+	});
+
+	commentInput.val(""); // 인풋 필드를 깨끗하게 비워준다
 }
 
 // (5) 댓글 삭제
